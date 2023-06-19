aa8454751239d8f66617aca463a1b02a1ce3d3fd
samyro14 <72630697+samyro14@users.noreply.github.com>
2023-06-19 22:30:11 +0300

    first commit

---
+++ gun/gun.ino
@@ -0,0 +1,14 @@
+#include <deprecated.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
